c4969055e9d262af535074326eaa84a9cb2aaeb3
Jonathan Edradan <jmedradan@gmail.com>
2014-11-20 16:58:32 -0800

    Adding DOM files

--- assignments/week-1/dom-exercises/traffic-lights/dom.js
+++ assignments/week-1/dom-exercises/traffic-lights/dom.js
@@ -1,1 +1,30 @@
 // write code here
+var red = function() {
+	document.querySelector("#light1").setAttribute("fill", "red");
+}
+
+var white = function() {
+	document.querySelector("#light1").setAttribute("fill", "white");
+}
+
+document.querySelector("#light1").addEventListener("mouseover", red);
+document.querySelector("#light1").addEventListener("mouseout", white);
+
+var yellow = function() {
+	document.querySelector("#light2").setAttribute("fill", "yellow");
+}
+
+var white = function() {
+	document.querySelector("#light2").setAttribute("fill", "white");
+}
+document.querySelector("#light2").addEventListener("mouseover", yellow);
+document.querySelector("#light2").addEventListener("mouseout", white);
+
+var green = function() {
+	document.querySelector("#light3").setAttribute("fill", "green")
+}
+
+
+document.querySelector("#light3")addEventListener("mouseover", function() {
+	document.querySelector("#light3").setAttribute("fill", "green")
+}
