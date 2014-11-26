81c68c7b2fa288ba18eb29b6abc769aedd9a057a
Jonathan Edradan <jmedradan@gmail.com>
2014-11-25 16:35:16 -0800

    Submitting Hack-it assignment

---
+++ assignments/week-1/dom-exercises/hack-it/edradan-jonathan/doms.js
@@ -0,0 +1,10 @@
+// URL: http://aerogeeks.com/
+
+document.querySelector("#post-4631").children[0].children[0].children[0].src ="http://i62.tinypic.com/20k9i77.jpg";
+"http://i62.tinypic.com/20k9i77.jpg"
+
+document.querySelector("#post-4631").children[1].children[0].children[0].textContent = "Jonathan Edradan leads the pack at Ironman 70.3 Cebu"
+
+document.querySelector("#post-4631").children[2].children[0].textContent = "Amateur triathlete, Jonathan Edradan, stuns the crowd at Ironman 70.3 Cebu with a stunning performace yesterday afternoon. San Francisco native [...]"
+
+//edradan-jonathan
